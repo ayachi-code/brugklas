0b18d379aaab7a73ea6b7b5de351fe125a30ebd9
bilal <buddygames.bilal@gmail.com>
2017-10-07 16:13:26 +0200

    jumbotron toegevoegt

--- index.php
+++ index.php
@@ -19,16 +19,24 @@
 
 <body>
 
-  <nav class="navbar navbar-default" style="background-color: #ffbb3f">
+  <nav class="navbar navbar-default" style="background-color: rgb(217, 145, 0)">
      <div class="container-fluid">
        <div class="navbar-header">
          <a class="navbar-brand" href="/index.html">School inschrijven</a>
        </div>
-       <ul class="nav navbar-nav" style="background-color: rgb(77, 101, 141);">
-
+       <ul class="nav navbar-nav" style="background-color: rgb(255, 198, 83);">
+             <li class="active"><a href="/index.php" style="background-color: rgb(255, 198, 83)">Home</a></li>
+              <li class="active"><a href="/index.php" style="background-color: rgb(255, 198, 83)">inschrijven</a></li>
+               <li class="active"><a href="/index.php" style="background-color: rgb(255, 198, 83)">over ons</a></li>
        </ul>
      </div>
    </nav>
+
+
+   <div class="jumbotron text-center" style="margin-top: -20px;">
+      <h1>Welkom bij brugklasinschrijven.nl</h1>
+    </div>
+
 
 
 
