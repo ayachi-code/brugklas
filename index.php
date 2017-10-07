d719dbd900e9e04e0b84ed877641ad8ceaeaa053
bilal <buddygames.bilal@gmail.com>
2017-10-07 16:01:09 +0200

    bootstrap libary toegevoegt

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
-
+<link rel="stylesheet" href="/css/home/index.css">
 
 
 
@@ -18,6 +18,9 @@
 
 
 <body>
+
+
+
 
 
 </body>
